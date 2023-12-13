afaf45b714d22224262c8af5660e445897624b4e
Tony5300 <tony_tolen@ziggo.nl>
2023-12-13 16:16:37 +0100

    feat: added a new function to filter through the jobs, only need to make it also visible to the DOM.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
+import { useState } from "react";
 import JobCard from "./components/JobCard/JobCard";
 import data from "./data/data.json";
+import "./index.css";
 
 function App() {
-  console.log(`data:`, data);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  console.log(`selectedTags:`, selectedTags);
+
+  // function to filter through the array of data.
+  const handleSelectedTags = (tag: string) => {
+    // check if the tag is already is selected.
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(
+        // If it is selected, remove it from the selected tags Array
+        selectedTags.filter((selectedTag) => selectedTag !== tag)
+      );
+    } else {
+      // If it is not selected, add it to the selected tag array
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  // const filteredJobs = data.filter((jobData) => {
+  //   // Check if any of the selected tags are present in languages or tools or role or level
+  //   return selectedTags.some(
+  //     // Using the some method to iterate through selectedTags
+  //     // The some method returns true if the condition is met for at least one tag
+  //     (tag) =>
+  //       jobData.languages.includes(tag) ||
+  //       jobData.tools.includes(tag) ||
+  //       jobData.role.includes(tag) ||
+  //       jobData.level.includes(tag) ||
+  //       // If there are no selected tags (length of selectedTags is zero), return the entire array and show all the items on the DOM
+  //       selectedTags.length === 0
+  //   );
+  // });
+
   return (
     <>
+      <header className="header-background-image-container">
+        <img className="header-background" />
+      </header>
       {Array.isArray(data) ? (
         data.map((jobData) => {
-          return <JobCard key={jobData.id} jobData={jobData} />;
+          return (
+            <JobCard
+              key={jobData.id}
+              jobData={jobData}
+              handleSelectedTags={handleSelectedTags}
+            />
+          );
         })
       ) : (
         <div>Oops there went something wrong</div>
--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,3 +1,5 @@
+import "./JobCardStyles.css";
+
 // JobDataProps describes the expected shape of an object representing job data. It includes properties like id, company, logo, etc.
 type JobDataProps = {
   id: number;
@@ -18,34 +20,77 @@ type JobDataProps = {
 //JobCardProps specifies that the jobData prop passed to JobCard should conform to the JobDataProps type.
 type JobCardProps = {
   jobData: JobDataProps;
+
+  // tell TS that it expects one argument, and that argument should be a string.
+  handleSelectedTags: (tag: string) => void;
 };
 
-const JobCard: React.FC<JobCardProps> = ({ jobData }) => {
+const JobCard: React.FC<JobCardProps> = ({ jobData, handleSelectedTags }) => {
   return (
-    <section>
-      <div className="img-container">
-        <img src={jobData.logo} className="img" alt="company-logo" />
-      </div>
-      <ul className="job-info-container">
-        <li>{jobData.company}</li>
-        <li>{jobData.new ? "new".toUpperCase() : null}</li>
-        <li>{jobData.featured ? "featured".toUpperCase() : null}</li>
-      </ul>
-      <p>{jobData.position}</p>
-      <ul className="job-specification-container">
-        <li>{jobData.postedAt}</li>
-        <li>{jobData.contract}</li>
-        <li>{jobData.location}</li>
-      </ul>
-      <div className="job-tags-container">
-        <p>{jobData.role}</p>
-        <p>{jobData.level}</p>
-        {jobData.languages.map((lang) => {
-          return <li key={lang}>{lang}</li>;
-        })}
-        {jobData.tools.map((tool) => {
-          return <li key={tool}>{tool}</li>;
-        })}
+    <section className="card-outer-container">
+      <div className="card-container">
+        <div className="card-company-logo-container">
+          <img
+            src={jobData.logo}
+            alt="company-logo"
+            className="card-company-logo"
+          />
+        </div>
+        <section className="card-inner-container">
+          <ul className="card-header-info">
+            <li className="card-company-name">{jobData.company}</li>
+            <li className={jobData.new ? "card-new-tag" : ""}>
+              {jobData.new ? "new!".toUpperCase() : ""}
+            </li>
+            <li className={jobData.featured ? "card-featured-tag" : ""}>
+              {jobData.featured ? "featured".toUpperCase() : ""}
+            </li>
+          </ul>
+          <p className="card-job-position">{jobData.position}</p>
+          <ul className="card-job-details">
+            <li className="job-details">{jobData.postedAt}</li>
+            <li className="job-details">{jobData.contract}</li>
+            <li className="job-details">{jobData.location}</li>
+          </ul>
+          <hr />
+          <ul className="card-job-work-info">
+            <li
+              className="job-info"
+              onClick={() => handleSelectedTags(jobData.role)}
+            >
+              {jobData.role}
+            </li>
+            <li
+              className="job-info"
+              // jobData.role is a string, so it fits with (tag: string).
+              onClick={() => handleSelectedTags(jobData.level)}
+            >
+              {jobData.level}
+            </li>
+            {jobData.languages.map((lang, id) => {
+              return (
+                <li
+                  key={id}
+                  className="job-info"
+                  onClick={() => handleSelectedTags(lang)}
+                >
+                  {lang}
+                </li>
+              );
+            })}
+            {jobData.tools.map((tool, id) => {
+              return (
+                <li
+                  key={id}
+                  className="job-info"
+                  onClick={() => handleSelectedTags(tool)}
+                >
+                  {tool}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
       </div>
     </section>
   );
